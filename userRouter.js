cc9390c38578d30e1aad1f644d69e30e4ffc5e83
luoqiming82 <qimingluo28@gmail.com>
2023-06-27 12:37:47 +0800

    finish post exercise, pass self test

--- userRouter.js
+++ userRouter.js
@@ -14,4 +14,40 @@ router.post('/users', function(req, res){
     })
 })
 
+router.post('/users/:_id/exercises', function(req, res){
+    const id = req.params._id;
+    const description = req.body.description;
+    const duration = req.body.duration;
+    const date = req.body.date;
+    
+    // convert date to unix timestamp
+    let unixDate;
+    if(!date){
+        unixDate = Date.now();
+    }else{
+        unixDate = Date.parse(date);
+    }
+    // store data to db . sqlite foreign key must be manully enforced!
+    // because user_id is a foreign key, so if the posted _id doesn't existed in users table, insert query will fail, so no need to check if _id existed
+    db.run('PRAGMA foreign_keys = on', function(){
+        db.get('insert into user_exercise values (?,?,?,?) returning *', [id, description, duration, date], function(err, row){
+            if(err){
+                res.status(400).json({error: 'please check your _id'});
+            }else{
+                const rowInserted = row;
+                db.get('select username from users where _id = ?', [id], function(err, row){
+                    res.send({
+                        username: row.username,
+                        description: rowInserted.description,
+                        duration: rowInserted.duration,
+                        date: (new Date(rowInserted.date)).toDateString(),
+                        _id: Number(id)
+                    })
+                    
+                })
+            }
+        });
+    })
+})
+
 module.exports = router
